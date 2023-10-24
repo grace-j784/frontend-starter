05db16b32f92fbb7bab290932876d4833fe815e3
Grace <gsjau@mit.edu>
2023-10-24 19:10:05 -0400

    updated save page

--- client/components/Savour/SavedPostListComponent.vue
+++ client/components/Savour/SavedPostListComponent.vue
@@ -10,8 +10,13 @@ const { isLoggedIn, currentUsername } = storeToRefs(useUserStore());
 
 const loaded = ref(false);
 let posts = ref<Array<Record<string, string>>>([]);
+let post_tags = ref<Array<Record<string, string>>>([]);
+let show_tags_post_id = ref("");
 let editing = ref("");
+//let tagging = ref("");
+//let deleting_tag = ref("");
 let searchAuthor = ref("");
+//let searchTag = ref("");
 
 async function getSavedPosts(author?: string) {
   let query: Record<string, string> = author !== undefined ? { author } : {};
@@ -27,6 +32,18 @@ async function getSavedPosts(author?: string) {
 
 function updateEditing(id: string) {
   editing.value = id;
+}
+
+async function seeTags(post_id: string) {
+  let query: Record<string, string> = { post_id: post_id };
+  let postResults;
+  try {
+    postResults = await fetchy("/api/tags/tagged/:post", "GET", { query });
+  } catch (_) {
+    return;
+  }
+  show_tags_post_id.value = post_id;
+  post_tags.value = postResults;
 }
 
 onBeforeMount(async () => {
@@ -55,6 +72,11 @@ async function UnsavePost(id: string) {
       <PostComponent v-if="editing !== post._id" :post="post" @refreshPosts="getSavedPosts" @editPost="updateEditing" />
       <EditPostForm v-else :post="post" @refreshPosts="getSavedPosts" @editPost="updateEditing" />
       <button class="btn-small pure-button" @refreshPosts="getSavedPosts" @click="UnsavePost(post._id)">Unsave</button>
+      <button class="btn-small pure-button" @click="seeTags(post._id)">See Tags</button>
+      <menu v-if="show_tags_post_id == post._id">
+        <article v-for="tag in post_tags" :key="tag._id">{{ tag.tag_name }}</article>
+        <article v-if="post_tags.length == 0">Post has no tags</article>
+      </menu>
     </article>
   </section>
   <p v-else-if="loaded">No posts found or user not logged in</p>
@@ -93,5 +115,14 @@ article {
   justify-content: space-between;
   margin: 0 auto;
   max-width: 60em;
+}
+
+menu {
+  list-style-type: none;
+  display: flex;
+  flex-direction: row;
+  gap: 1em;
+  padding: 0;
+  margin: 0;
 }
 </style>
